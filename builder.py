5bc6f466c14d5a9cc12e8f0fb4a15374711fa1c8
Claudio D. Catalani <catalaniclaudiodamian@gmail.com>
2022-04-24 14:25:02 -0400

    Singleton, Prototype, Builder

---
+++ builder.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 13:47:47 2022
+
+@author: q
+
+classes
+
+    Director
+        Construct
+    Builder
+        Build
+    Product
+        Parts
+        
+"""
+import time
+
+class Body():
+    model = "SUV"    
+    def __repr__(self):
+        return type(self).model
+
+class Engine():
+    model = "V8"    
+    def __repr__(self):
+        return type(self).model
+
+class Wheel():
+    model = "34x12.50R18LT"    
+    def __repr__(self):
+        return type(self).model
+
+class Jeep():
+    
+    def __init__(self):
+        self.id = time.time_ns()
+        self.body = None
+        self.engine = None
+        self.wheels = []
+        pass
+        
+    def __repr__(self):
+        name = 'Mod.: ' + type(self).__name__
+        bodyn = '\nId.: ' + str(self.id)
+        body = '\nBody : ' + str(self.body)
+        engine = '\nEngine : ' + str(self.engine)
+        wheels = '\nWheels : ' + str(self.wheels)
+        return name + bodyn + body + engine + wheels 
+    
+class Builder():
+    
+    def reset(self):
+        pass
+    
+    def addPart(self):
+        pass
+
+    def getProduct(self):
+        pass
+
+class JeepBuilder(Builder):
+    
+    def __init__(self):
+        pass
+
+    def reset(self):
+        self._product = Jeep()        
+        pass
+    
+    def addPart(self, part):
+        
+        if isinstance(part, Body):
+            self._product.body = part
+        
+        elif isinstance(part, Engine):    
+            self._product.engine = part
+            
+        elif isinstance(part, Wheel):
+            self._product.wheels.append(part)
+        pass
+    
+    def getProduct(self):
+        return self._product
+            
+    
+class Director():
+    
+    __builder = None
+    
+    def __init__(self, builder : Builder):
+        self.__builder = builder()
+        pass
+    
+    def getProduct(self):
+        
+        self.__builder.reset()
+
+        # car body
+        self.__builder.addPart(part = Body())
+       
+        
+        # car engine
+        self.__builder.addPart(part = Engine())
+        
+        # 4 wheels
+        for i in range(4):
+            self.__builder.addPart(part = Wheel())
+            
+        return self.__builder.getProduct()    
+        
+    
+    
+if __name__ == '__main__':
+    
+    dt = Director(builder = JeepBuilder)
+    
+    dt.getProduct()
+
+    dt.getProduct() 
+    
